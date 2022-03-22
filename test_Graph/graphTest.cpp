fa4296dcd8180969884f919b1e49c466532d5a72
Dieupix <79543909+Dieupix@users.noreply.github.com>
2022-03-22 21:09:14 +0100

    Classe Graph: structuration des tests

--- test_Graph/graphTest.cpp
+++ test_Graph/graphTest.cpp
@@ -4,9 +4,29 @@
 #include "doctest.h"
 #include "../Graph/graph.h"
 
-TEST_CASE("2 - Test of the class Graph")
+TEST_CASE("2 - Tests of the class Graph")
 {
+    SUBCASE("Tests of the constructors")
+    {
+        SUBCASE("Constructor with FS and APS")
+        {
+        }
 
+        SUBCASE("Constructor with matAdj")
+        {
+        }
+    }
+
+    SUBCASE("Tests of the public functions")
+    {
+        SUBCASE("FS_APS_to_MatAdj()")
+        {
+        }
+
+        SUBCASE("matAdj_to_FS_APS()")
+        {
+        }
+    }
 }
 
 #endif // GRAPHTEST_H
