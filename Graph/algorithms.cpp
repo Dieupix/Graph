70a834b5ad352f42351b2d6cd7118664f00221da
Dieupix <79543909+Dieupix@users.noreply.github.com>
2022-03-30 15:10:44 +0200

    Algorithms: Corriger Tarjan

--- Graph/algorithms.cpp
+++ Graph/algorithms.cpp
@@ -131,6 +131,75 @@ void printVector(const vector<int>& v)
     cout << v[i] << "]\n";
 }
 
+void Prufer_encode(vector<vector<int>> mat, vector<int>& p)
+{
+    unsigned n = mat[0][0];
+    p.clear();
+    p.resize(n-1);
+    p[0] = n-2;
+
+    for(unsigned i = 1; i <= n; ++i)
+    {
+        mat[i][0] = 0;
+        for(unsigned j = 1; j <= n; ++j)
+        {
+            mat[i][0] += mat[i][j];
+        }
+    }
+
+    for(unsigned k = 1; k <= n-2; ++k)
+    {
+        int i = 1, j = 1;
+        while(mat[i][0] != 1) ++i;
+        while(mat[i][j] != 1) ++j;
+
+        p[k] = j;
+        mat[i][j] = 0;
+        mat[j][i] = 0;
+        mat[i][0] = 0;
+        --mat[j][0];
+    }
+}
+
+void Prufer_decode(const vector<int>& p, vector<vector<int>>& mat)
+{
+    unsigned m = p[0], n = m + 2;
+    vector<int> s(m + 3);
+
+    mat.clear();
+    mat.resize(n + 1);
+    for(unsigned i = 0; i <= n; ++i) mat[i].resize(n + 1, 0);
+
+    for(unsigned i = 1; i <= n; ++i) s[i] = 0;
+    for(unsigned i = 1; i <= m; ++i) ++s[p[i]];
+
+    for(unsigned i = 1; i <= m; ++i)
+    {
+        unsigned k = 1;
+        while(s[k] != 0) ++k;
+
+        mat[p[i]][k] = 1;
+        mat[k][p[i]] = 1;
+
+        s[k] = -1;
+        --s[p[i]];
+    }
+
+    int k = -1;
+    for(unsigned i = 1; i <= n; ++i)
+    {
+        if(s[i] == 0)
+        {
+            if(k == -1) k = i;
+            else
+            {
+                mat[i][k] = 1;
+                mat[k][i] = 1;
+            }
+        }
+    }
+}
+
 void rang(vector<int>& rang, const vector<int>& fs, const vector<int>& aps)
 {
         int n = aps[0];
--- Graph/algorithms.h
+++ Graph/algorithms.h
@@ -14,6 +14,8 @@ void empiler                (int x, vector<int>& pilch);
 void fortconnexe            (const vector<int>& FS, const vector<int>& APS, vector<int>& cfc, vector<int>& pilch, vector<int>& pred, vector<int>& prem);
 void mat_distance           (const vector<int>& FS, const vector<int>& APS, vector<vector<int>>& matriceDistance);
 void printVector            (const vector<int>& vector);
+void Prufer_encode          (vector<vector<int>> mat, vector<int>& p);
+void Prufer_decode          (const vector<int>& p, vector<vector<int>>& mat);
 void rang                   (vector<int>& rang, const vector<int>& FS, const vector<int>& APS);
 void rangToString           (const vector<int>& FS, const vector<int>& APS);
 void Tarjan                 (const vector<int>& FS, const vector<int>& APS);
